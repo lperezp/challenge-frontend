82236c7ceece3b5ebcce3d6033854cf0405f736c
Luis Eduardo <luiseperezp.10@gmail.com>
2021-04-11 12:50:27 -0500

    feat(shared): add shared module and constant variables

---
+++ src/app/shared/app.settings.ts
@@ -0,0 +1,12 @@
+export class AppSettings {
+  public static MENU = [
+    {
+      label: 'Registro de Clientes',
+      routerlink: '/client',
+    },
+    {
+      label: 'Listado de Clientes',
+      routerlink: 'list-client',
+    },
+  ];
+}
